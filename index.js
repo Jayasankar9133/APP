dac0e0b7940d79776428c988c6f2b57f2212f053
Jayasankar <sankarjaya443@gmail.com>
2023-11-23 18:59:03 +0530

    APP

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = process.port | 4200;
+
+app.listen(port, () => {
+    console.log("Server Running");
+});
+
+
+app.get("/", (req, res) => {
+    res.send("My Name is Jayasankar");
+});
